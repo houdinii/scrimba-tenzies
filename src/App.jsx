10759f6ad8b0abd87cb509862dfdca6265ac39e4
Brian Barnes Jr <houdinii1984@gmail.com>
2023-04-07 17:08:20 -0600

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './App.css'
+
+/**
+ * Challenge: Start a brand-new React app!
+ * - Create a separate App component
+ * - Import and render the App component here
+ * - In the App component, render a <main> element
+ * - Style everything to look like the slide
+ */
+
+function App() {
+  return (
+    <div className="App">
+
+    </div>
+  )
+}
+
+export default App
