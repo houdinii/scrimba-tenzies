59aba387c86ef90ffa9b6e8fb131d14dc211cd8f
Brian Barnes Jr <houdinii1984@gmail.com>
2023-04-07 17:43:47 -0600

    style title, instructions, dice, and button

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './App.css'
+import Die from "../components/Die.jsx";
 
 /**
  * Challenge:
@@ -16,7 +17,31 @@ import './App.css'
 function App() {
   return (
     <div className="App">
-      <main className={"container"}></main>
+      <main className={"container"}>
+        <header>
+          <h1 className={"header--title"}>Tenzies</h1>
+          <p className={"header--instructions"}>Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
+        </header>
+        <section className={"dice"}>
+          <div className={"dice--row"}>
+            <Die selected={true} value={1}/>
+            <Die selected={false} value={2}/>
+            <Die selected={true} value={1}/>
+            <Die selected={false} value={4}/>
+            <Die selected={false} value={5}/>
+          </div>
+          <div className={"dice--row"}>
+            <Die selected={false} value={3}/>
+            <Die selected={false} value={3}/>
+            <Die selected={false} value={5}/>
+            <Die selected={false} value={6}/>
+            <Die selected={true} value={1}/>
+          </div>
+        </section>
+        <section className={"buttons"}>
+          <button className={"buttons--roll-btn"}>Roll</button>
+        </section>
+      </main>
     </div>
   )
 }
