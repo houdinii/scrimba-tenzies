c6daf710613baf8a5413553841e9ece584843e9d
Brian Barnes Jr <houdinii1984@gmail.com>
2023-04-07 17:54:42 -0600

    finished style

--- components/Die.jsx
+++ components/Die.jsx
@@ -1,19 +1,8 @@
 import React from 'react'
-/**
- * Challenge:
- *
- * - Create a Die component that takes a `value` prop
- * - Render 10 instances of the Die component (manually)
- *      - Provide a number between 1-6 for the value on each
- *        for now
- * - Style the <main> and <Die> components
- *   to look like they do in the slide
- */
-
 export default function Die(props){
   return(
       <div className={`die ${ props.selected ? "selected" : "" }`}>
-        <p>{props.value}</p>
+        <p className={"die--num"}>{props.value}</p>
       </div>
   )
 }
--- src/App.jsx
+++ src/App.jsx
@@ -2,25 +2,15 @@ import React from 'react'
 import './App.css'
 import Die from "../components/Die.jsx";
 
-/**
- * Challenge:
- *
- * - Create a Die component that takes a `value` prop
- * - Render 10 instances of the Die component (manually)
- *      - Provide a number between 1-6 for the value on each
- *        for now
- * - Style the <main> and <Die> components
- *   to look like they do in the slide
- */
-
-
 function App() {
   return (
     <div className="App">
       <main className={"container"}>
         <header>
           <h1 className={"header--title"}>Tenzies</h1>
-          <p className={"header--instructions"}>Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
+          <p className={"header--instructions"}>
+            Roll until all dice are the same. Click each die to freeze it at its current value between rolls.
+          </p>
         </header>
         <section className={"dice"}>
           <div className={"dice--row"}>
