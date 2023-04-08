500fcb65578a7037edaab79f94d2cd1111266df1
Brian Barnes Jr <houdinii1984@gmail.com>
2023-04-07 19:36:24 -0600

    dice functional but not held on roll

--- components/Die.jsx
+++ components/Die.jsx
@@ -1,7 +1,20 @@
 import React from 'react'
 export default function Die(props){
+  /**
+   * Challenge: Create a function `holdDice` that takes
+   * `id` as a parameter. For now, just have the function
+   * console.log(id).
+   *
+   * Then, figure out how to pass that function down to each
+   * instance of the Die component so when each one is clicked,
+   * it logs its own unique ID property. (Hint: there's more
+   * than one way to make that work, so just choose whichever
+   * you want)
+   *
+   */
+
   return(
-      <div className={`die ${ props.selected ? "selected" : "" }`}>
+      <div className={`die ${props.isHeld ? "selected" : ""}`} onClick={ props.holdDice }>
         <p className={"die--num"}>{props.value}</p>
       </div>
   )
--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,45 @@
 import React from 'react'
 import './App.css'
 import Die from "../components/Die.jsx";
+import {nanoid} from "nanoid";
 
 function App() {
-  return (
+  function allNewDice(){
+    let arr = [];
+    for(let i=0; i<10; i++){
+      arr.push({
+        value: Math.floor(Math.random() * 6) + 1,
+        isHeld: false,
+        id: nanoid()
+      })
+    }
+    return arr
+  }
+  const [dice, setDice] = React.useState(allNewDice())
+  const rollDice = () => setDice(allNewDice())
+
+  function holdDice(id){
+
+    setDice(dice.map(die => {
+      return die.id === id
+        ? {
+          id: die.id,
+          value: die.value,
+          isHeld: !die.isHeld
+        } : die
+    }))
+  }
+
+  let diceEls = dice.map(die =>
+      <Die
+          value={die.value}
+          isHeld={die.isHeld}
+          key={die.id}
+          holdDice={() => holdDice(die.id)}
+      />
+  )
+
+    return (
     <div className="App">
       <main className={"container"}>
         <header>
@@ -13,23 +49,10 @@ function App() {
           </p>
         </header>
         <section className={"dice"}>
-          <div className={"dice--row"}>
-            <Die selected={true} value={1}/>
-            <Die selected={false} value={2}/>
-            <Die selected={true} value={1}/>
-            <Die selected={false} value={4}/>
-            <Die selected={false} value={5}/>
-          </div>
-          <div className={"dice--row"}>
-            <Die selected={false} value={3}/>
-            <Die selected={false} value={3}/>
-            <Die selected={false} value={5}/>
-            <Die selected={false} value={6}/>
-            <Die selected={true} value={1}/>
-          </div>
+          {diceEls}
         </section>
         <section className={"buttons"}>
-          <button className={"buttons--roll-btn"}>Roll</button>
+          <button className={"buttons--roll-btn"} onClick={rollDice}>Roll</button>
         </section>
       </main>
     </div>
