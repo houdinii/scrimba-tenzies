56f41f987ed6ea6083fc1b50eeb98c082d4d09e8
Brian Barnes Jr <houdinii1984@gmail.com>
2023-04-07 20:15:51 -0600

    Finished project

--- src/App.jsx
+++ src/App.jsx
@@ -2,46 +2,82 @@ import React from 'react'
 import './App.css'
 import Die from "../components/Die.jsx";
 import {nanoid} from "nanoid";
+import Confetti from "react-confetti";
+
+
+/*
+* Extra Credit Ideas:
+* CSS: Put real dots on the dice
+* JS: Track the number of rolls
+* JS: Track how much time it took
+* localStorage: Save best time
+*
+* */
 
 function App() {
+  const generateNewDie = () => {
+    return {
+      value: Math.floor(Math.random() * 6) + 1,
+      isHeld: false,
+      id: nanoid()
+    }
+  }
+
   function allNewDice(){
     let arr = [];
     for(let i=0; i<10; i++){
-      arr.push({
-        value: Math.floor(Math.random() * 6) + 1,
-        isHeld: false,
-        id: nanoid()
-      })
+      arr.push(generateNewDie())
     }
     return arr
   }
+
   const [dice, setDice] = React.useState(allNewDice())
-  const rollDice = () => setDice(allNewDice())
+  const [tenzies, setTenzies] = React.useState(false)
+
+  React.useEffect(() => {
+    let valid = true
+    const checkValue = dice[0].value
+
+    dice.forEach(die => die.value !== checkValue || !die.isHeld ? valid = false : "")
+
+    if(valid){
+      setTenzies(true)
+      console.log("You Win!")
+    }
+  }, [dice])
+
+  const rollDice = () => {
+    if(tenzies) {
+      setTenzies(false)
+      setDice(allNewDice())
+    } else {
+      setDice(dice.map(die => !die.isHeld ? generateNewDie() : die))
+    }
+  }
 
   function holdDice(id){
-
     setDice(dice.map(die => {
       return die.id === id
         ? {
-          id: die.id,
-          value: die.value,
+          ...die,
           isHeld: !die.isHeld
         } : die
     }))
   }
 
   let diceEls = dice.map(die =>
-      <Die
-          value={die.value}
-          isHeld={die.isHeld}
-          key={die.id}
-          holdDice={() => holdDice(die.id)}
-      />
+    <Die
+      value={die.value}
+      isHeld={die.isHeld}
+      key={die.id}
+      holdDice={() => holdDice(die.id)}
+    />
   )
 
     return (
     <div className="App">
       <main className={"container"}>
+        {tenzies &&  <Confetti/>}
         <header>
           <h1 className={"header--title"}>Tenzies</h1>
           <p className={"header--instructions"}>
@@ -52,7 +88,7 @@ function App() {
           {diceEls}
         </section>
         <section className={"buttons"}>
-          <button className={"buttons--roll-btn"} onClick={rollDice}>Roll</button>
+          <button className={"buttons--roll-btn"} onClick={rollDice}>{tenzies ? "New Game" : "Roll"}</button>
         </section>
       </main>
     </div>
